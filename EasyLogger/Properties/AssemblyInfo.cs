06e1e8d9d886626d3a6b76a7a9ad1440553696c4
Nima Ara <NimaAra@users.noreply.github.com>
2016-01-19 12:15:47 +0000

    Updated Assembly Version
    In preparation of the first release.

--- EasyLogger/Properties/AssemblyInfo.cs
+++ EasyLogger/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("EasyLogger.Tests.Unit")]
