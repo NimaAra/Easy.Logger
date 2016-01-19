9ddf4bf9e10941a948cb7db11ee0091724ac56a3
Nima Ara <NimaAra@users.noreply.github.com>
2016-01-19 12:16:17 +0000

    Update AssemblyInfo.cs

--- EasyLogger/Properties/AssemblyInfo.cs
+++ EasyLogger/Properties/AssemblyInfo.cs
@@ -3,8 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("EasyLogger")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: NeutralResourcesLanguage("en-GB")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
