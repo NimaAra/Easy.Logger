d133cfb2545d9886e0f0f1be69030a4bc230018e
Nima Ara <nima.ara@inbox.com>
2016-06-28 21:59:14 +0100

    Added XML documentation

--- Easy.Logger/LogLevel.cs
+++ Easy.Logger/LogLevel.cs
@@ -5,10 +5,29 @@
     /// </summary>
     public enum LogLevel
     {
+        /// <summary>
+        /// The debug level.
+        /// </summary>
         Debug = 0,
+
+        /// <summary>
+        /// The information level.
+        /// </summary>
         Info,
+
+        /// <summary>
+        /// The warning level.
+        /// </summary>
         Warn,
+
+        /// <summary>
+        /// The error level.
+        /// </summary>
         Error,
+
+        /// <summary>
+        /// The fatal level.
+        /// </summary>
         Fatal
     }
 }
