a2ff4e0150f21a02868bc0223b332cbbfcaf8f84
Nima Ara <NimaAra@users.noreply.github.com>
2019-07-31 11:42:06 +0100

    Cache instances of loggers when creating them in EasyLoggerProvider.

--- Easy.Logger.Extensions.Microsoft/EasyLoggerProvider.cs
+++ Easy.Logger.Extensions.Microsoft/EasyLoggerProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace Easy.Logger.Extensions.Microsoft
 {
+    using System.Collections.Concurrent;
     using System.Diagnostics.CodeAnalysis;
     using global::Microsoft.Extensions.Logging;
 
@@ -9,6 +10,9 @@
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public sealed class EasyLoggerProvider : ILoggerProvider
     {
+        private readonly ConcurrentDictionary<string, EasyLogger> _loggerCache = 
+            new ConcurrentDictionary<string, EasyLogger>();
+        
         /// <summary>
         /// Creates an instance of the <see cref="EasyLoggerProvider"/>.
         /// <remarks>A valid <c>log4net</c> configuration file must be present in the working directory.</remarks>
@@ -26,7 +30,7 @@
         /// Creates an <see cref="ILogger"/> with the given <paramref name="categoryName"/>.
         /// </summary>
         public ILogger CreateLogger(string categoryName) =>
-            new EasyLogger(Log4NetService.Instance.GetLogger(categoryName));
+            _loggerCache.GetOrAdd(categoryName, x => new EasyLogger(Log4NetService.Instance.GetLogger(x)));
 
         /// <summary>
         /// Disposes the instance flushing any pending log entries.
